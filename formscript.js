5b11ff3f0e671be42d3f6917f1bfe261cc145f52
nroge <32528011+HeyNoah@users.noreply.github.com>
2018-05-08 12:18:17 -0700

    communication submit - various additions and changes

---
+++ formscript.js
@@ -0,0 +1,17 @@
+function handleRegion() {
+    var theRegion = document.getElementById("selectRegion").value;
+    var theLocation = document.getElementById("selectLocation").value;
+    var newOptions = [];
+    for (var i=0; i<10; i++) {
+        newOption[i] = document.createElement("option");
+        
+    }
+
+    if (theRegion==="Eriador") {
+
+    }
+}
+
+/* find a possible way for the basic choosing of locations for region to work
+ * work with the for loops, variables, array, if states, etc
+ * */
