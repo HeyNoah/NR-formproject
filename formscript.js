98fb024652aa81de2e277d1110240b19a19e3b13
nroge <32528011+HeyNoah@users.noreply.github.com>
2018-05-10 12:52:16 -0700

    commit due to problems

--- formscript.js
+++ formscript.js
@@ -1,17 +1,45 @@
+var Locations = [];
+var LocationSelect = document.getElementById("selectLocation");
+var EriadorLocs = ["Shire", "Breeland", "Angmar", "Trollshaws"];
+var GondorLocs = ["Isengard", "Minas Tirith", "Dol Amroth", "Osgiliath", "Helms Deep"];
+var LindonLocs = ["Ossiriand", "Harlond", "Forlond"];
+var MordorLocs = ["Gorgoroth", "Barad Dur", "Durthang", "Narchost"];
+var RhovanianLocs = ["Erebor", "Dale", "Dol Guldur", "Fangorn"];
+var RohanLocs = ["Lothlorien", "Edoras", "Aldburg", "Upbourn", "Grimslade"];
+var newOption = [];
+
+// function clearSelects() {
+
+// }
+// clearSelects();
+
 function handleRegion() {
     var theRegion = document.getElementById("selectRegion").value;
-    var theLocation = document.getElementById("selectLocation").value;
-    var newOptions = [];
-    for (var i=0; i<10; i++) {
-        newOption[i] = document.createElement("option");
-        
+    // var theLocation = document.getElementById("selectLocation").value;
+
+    while (LocationSelect.length > 0) {
+        LocationSelect.remove(0);
+    }
+    while (newOption.length > 0) {
+        newOption.remove(0);
     }
 
     if (theRegion==="Eriador") {
-
+        Locations = EriadorLocs;
+    } else if (theRegion==="Gondor"){
+        Locations = GondorLocs;
+    } else if (theRegion==="Lindon") {
+        Locations = LindonLocs;
+    } else if (theRegion==="Mordor") {
+        Locations = MordorLocs;
+    } else if (theRegion==="Rhovanian") {
+        Locations = RhovanianLocs;
+    } else if (theRegion==="Rohan") {
+        Locations = RohanLocs;
+    }
+    for (var i=0; i<Locations.length; i++) {
+        newOption[i] = document.createElement("option");
+        newOption[i].text = Locations[i];
+        LocationSelect.add(newOption[i]);
     }
 }
-
-/* find a possible way for the basic choosing of locations for region to work
- * work with the for loops, variables, array, if states, etc
- * */
